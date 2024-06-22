d63d21e7a3ca6125ff70b211454c5c9d90e35602
jyoung28 <93066528+jyoung28@users.noreply.github.com>
2024-06-22 14:26:01 -0700

    changes

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,16 @@ from flask import Flask, request, jsonify
 import os
 import openai
 import config
+from openai import OpenAI
 
 openai_api_key = config.OPENAI_API_KEY
+
 app = Flask(__name__)
+
+client = OpenAI()
 
 # In-memory database for simplicity
 items = []
-
-openai_api_key = os.getenv('OPENAI_API_KEY')
 
 
 @app.route('/get_fit', methods=['GET'])
